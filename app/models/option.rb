b9ee86769d0fe3ec6f9c468de74bb5d042a42098
Budh Ram Gurung <budhram.gurung01@gmail.com>
2018-09-26 20:57:34 +0530

    attr_accessor to Option attributes

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,8 +1,8 @@
 class Option
   include ActiveModel::Model
 
-  attribute :no, Integer
-  attribute :content, :string
+  attr_accessor :no, :Integer
+  attr_accessor :content, :string
 
   validates :no, presence: true
   validates :content, presence: true

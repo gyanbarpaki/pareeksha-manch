dab54126e8b5776c8d236bda6d4de13ab4c592c0
Gyan Grg <gyankarmaa@gmail.com>
2018-10-10 06:46:14 +0530

    Actvie storage install

--- db/schema.rb
+++ db/schema.rb
@@ -10,33 +10,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180923084912) do
+ActiveRecord::Schema.define(version: 2018_10_10_005022) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "chapters", force: :cascade do |t|
-    t.text     "name"
-    t.text     "description"
-    t.integer  "subject_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text "name"
+    t.text "description"
+    t.integer "subject_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["subject_id"], name: "index_chapters_on_subject_id"
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string   "name"
-    t.string   "qns_type",     default: "Objective"
-    t.string   "difficulty",   default: "Medium"
-    t.string   "content"
-    t.string   "solution"
-    t.string   "option1"
-    t.string   "option2"
-    t.string   "option3"
-    t.string   "option4"
-    t.integer  "subject_id"
-    t.integer  "chapter_id"
-    t.integer  "topic_id"
-    t.integer  "sub_topic_id"
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
+    t.string "name"
+    t.string "qns_type", default: "Objective"
+    t.string "difficulty", default: "Medium"
+    t.string "content"
+    t.string "solution"
+    t.string "option1"
+    t.string "option2"
+    t.string "option3"
+    t.string "option4"
+    t.integer "subject_id"
+    t.integer "chapter_id"
+    t.integer "topic_id"
+    t.integer "sub_topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["chapter_id"], name: "index_questions_on_chapter_id"
     t.index ["sub_topic_id"], name: "index_questions_on_sub_topic_id"
     t.index ["subject_id"], name: "index_questions_on_subject_id"
@@ -44,44 +65,44 @@ ActiveRecord::Schema.define(version: 20180923084912) do
   end
 
   create_table "sub_topics", force: :cascade do |t|
-    t.text     "name"
-    t.text     "description"
-    t.integer  "topic_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text "name"
+    t.text "description"
+    t.integer "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["topic_id"], name: "index_sub_topics_on_topic_id"
   end
 
   create_table "subjects", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "topics", force: :cascade do |t|
-    t.text     "name"
-    t.text     "description"
-    t.integer  "chapter_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text "name"
+    t.text "description"
+    t.integer "chapter_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["chapter_id"], name: "index_topics_on_chapter_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
